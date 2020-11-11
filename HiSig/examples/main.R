2620e5651ff99bc19036eb2bc474902599a02360
fanzheng10 <fanzheng1101@gmail.com>
2020-11-11 12:39:51 -0800

    R package 0.0.9

---
+++ HiSig/examples/main.R
@@ -0,0 +1,13 @@
+library(HiSig)
+
+genes = readLines('target.txt')
+terms = readLines('tf.txt')
+data <- load_data('conn.txt', 'response.txt')
+
+hisig_out <- hisig_fit(data)
+beta_max_rand <- hisig_fit_rand(data, hisig_out$lambda, batch=100)
+
+impact <- cbind(hisig_out$beta.max, beta_max_rand)
+
+df <- parse_hisig(data, impact, term.names = terms, gene.names = genes)
+output(df, 'hisig_summary.txt')
