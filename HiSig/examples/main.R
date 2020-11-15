672a62616a7522db94768d9e3334203a1cd86923
fanzheng10 <fanzheng1101@gmail.com>
2020-11-14 20:26:07 -0800

    Use args in main.R

--- HiSig/examples/main.R
+++ HiSig/examples/main.R
@@ -1,13 +1,25 @@
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages(library(argparse))
 library(HiSig)
 
-genes = readLines('target.txt')
-terms = readLines('tf.txt')
-data <- load_data('conn.txt', 'response.txt', genes=genes, terms=terms)
+parser <- ArgumentParser()
+parser$add_argument("-x", required=T, help="the design matrix")
+parser$add_argument("-y", required=T, help="the response vector")
+parser$add_argument("-g", required=T, help='the gene list')
+parser$add_argument("-t", required=T, help="the system list")
+parser$add_argument("-o", required=T, help="the output name")
+parser$add_argument('--permute', default=1000, help='number of permutation')
+parser$add_argument('--nlambda', default=100, help='number of lambda sampled along the lasso path')
+args <- parser$parse_args()
 
-hisig_out <- hisig_fit(data)
-beta_max_rand <- hisig_fit_rand(data, hisig_out$lambda, batch=100)
+genes = readLines(args$g)
+terms = readLines(args$t)
+data <- load_data(args$x, args$y, genes=genes, terms=terms)
+
+hisig_out <- hisig_fit(data, nlambda=args$nlambda)
+beta_max_rand <- hisig_fit_rand(data, hisig_out$lambda, batch=args$permute/10)
 
 impact <- cbind(hisig_out$beta.max, beta_max_rand)
 
 df <- parse_hisig(data, impact, term.names = terms, gene.names = genes)
-output(df, 'hisig_summary.txt')
+output(df, paste0(args$o,'.txt'))
