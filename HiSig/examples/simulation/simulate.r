b0547257fdcdafe9546b55ca6c30d9a4fab590b0
fanzheng10 <fanzheng1101@gmail.com>
2021-02-10 23:26:48 -0800

    Now also possible to simulate Gaussian

--- HiSig/examples/simulation/simulate.r
+++ HiSig/examples/simulation/simulate.r
@@ -2,13 +2,14 @@ library(fabricatr)
 library(optparse)
 
 option_list = list( # don't use R -f, but Rscript
-  make_option(c("-n", "--nlayer"), 
+  make_option(c("-n", "--nlayer"),
               type="integer", default=4),
-  make_option(c("-m", "--size_per_layer"), 
+  make_option(c("-m", "--size_per_layer"),
               type="integer", default=5),
   make_option(c("-p", "--prob_pos"),
                 type="double", default=0.1),
-  make_option(c("--log"), action='store_true', default = T)
+  make_option(c("--log"), action='store_true'), # only applies when simulating poission distribution
+  make_option(c('--gaussian'), action='store_true') # if true, will simulate Gaussian distribution (with positive and negative)
 );
 
 opt_parser = OptionParser(option_list=option_list);
@@ -19,44 +20,75 @@ M = rep(opt$size_per_layer, nlayers)
 layer_names = paste0('L', 1:nlayers)
 p = opt$prob_pos
 
-hier_data <-
-  fabricate(
-    L = add_level(
-      N = M[[1]],
-      select = draw_binary(prob=p, N=N)
-      # weight = rpois(N, 1) * select,
-      # sum_weight = weight
+#local test
+# nlayers = 3
+# M = rep(2, nlayers)
+# layer_names = paste0('L', 1:nlayers)
+# p = 0.2
+
+if (opt$gaussian) {
+  hier_data <-
+    fabricate(
+      L = add_level(
+        N = M[[1]],
+        select = sample(c(draw_binary(prob=p, N=N/2), -1*draw_binary(prob=p, N=N/2)))
+      )
     )
-  )
+} else {
+  hier_data <-
+    fabricate(
+      L = add_level(
+        N = M[[1]],
+        select = draw_binary(prob=p, N=N)
+      )
+    )
+}
+
 names(hier_data)[names(hier_data) == 'L'] <- 'L1'
 names(hier_data)[names(hier_data) == 'select'] <- 'L1.select'
 # names(hier_data)[names(hier_data) == 'weight'] <- 'L1.weight'
 
 for (x in 2:nlayers) {
-  hier_data <-
-    fabricate(
-      data = hier_data,
-      L = add_level(
-        N = M[[x]],
-        select = draw_binary(prob=p, N=N) # make new selection
+
+  if (opt$gaussian) {
+    hier_data <-
+      fabricate(
+        L = add_level(
+          N = M[[1]],
+          select = sample(c(draw_binary(prob=p, N=N/2), -1*draw_binary(prob=p, N=N/2)))
+        )
       )
-    )
+  } else {
+    hier_data <-
+      fabricate(
+        L = add_level(
+          N = M[[1]],
+          select = draw_binary(prob=p, N=N)
+        )
+      )
+  }
   names(hier_data)[names(hier_data) == 'L'] <- paste0('L', x)
   names(hier_data)[names(hier_data) == 'select'] <- paste0('L', x,'.select')
   # names(hier_data)[names(hier_data) == 'weight'] <-paste0('L', x, '.weight')
-  
+
 }
 
-# add weights to selected 
+# add weights to selected
 hier_data$weight = 0
 N = nrow(hier_data)
 for (x in 1:nlayers) {
   cols = paste0('L', x, '.select')
-  weight = rpois(N, 1)* hier_data[[cols]] # this is ok, Poisson process is memoryless
+  if (opt$gaussian == F) {
+    weight = rpois(N, 1)* hier_data[[cols]] # this is ok, Poisson process is memoryless
+  }
+  else {
+    # flip half of the selected ones
+    weight = rnorm(N, mean=2, sd=1) * hier_data[[cols]] # need to set L.select pos and neg
+  }
   hier_data$weight <- hier_data$weight + weight
 }
 
-if (opt$log ==T) {
+if (opt$gaussian==F && opt$log ==T) {
   hier_data$weight <- log1p(hier_data$weight)
 }
 
@@ -66,15 +98,15 @@ write.conn<-function(nlayers) {
   # cluster names
   clust_names = c()
   for (i in 1:(nlayers-1)) {
-    coln = paste0('L', i) 
+    coln = paste0('L', i)
     clust_names <- c(clust_names, paste0('L', i, '_', unique(hier_data[[coln]])))
-  }  
+  }
 
   # write conn
   outf = 'sim_conn.txt'
   X<-paste0(1:nrow(hier_data)-1, "\t", 1:nrow(hier_data)-1)
   for (i in 1:(nlayers-1)) {
-    coln = paste0('L', i)  
+    coln = paste0('L', i)
     x = match( paste0('L', i, '_', hier_data[[coln]]), clust_names) + nrow(hier_data) -1
     x = paste0(1:nrow(hier_data)-1, "\t", x)
     X <-c(X, x)
@@ -82,13 +114,13 @@ write.conn<-function(nlayers) {
   filecon <- file(outf)
   writeLines(X, filecon)
   close(filecon)
-  
+
   # write terms
   outf = 'terms.txt'
   filecon <- file(outf)
   writeLines(clust_names, filecon)
   close(filecon)
-  
+
   # write truth
   sel_clusts = c()
   for (i in 1:(nlayers-1)) {
@@ -104,7 +136,7 @@ write.conn<-function(nlayers) {
   filecon <- file(outf)
   writeLines(sel_clusts, filecon)
   close(filecon)
-  
+
 }
 
 write.score <- function() {
