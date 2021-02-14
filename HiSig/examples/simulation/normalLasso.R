52bed5bf2695685efda087a9d48cacffc90033e9
fanzheng10 <fanzheng1101@gmail.com>
2021-02-14 15:50:18 -0800

    Fix normalLasso.R

--- HiSig/examples/simulation/normalLasso.R
+++ HiSig/examples/simulation/normalLasso.R
@@ -10,10 +10,10 @@ X <- as.matrix(read.table('sim_conn.txt', header=F))
 X_sp = sparseMatrix(X[,1], X[,2], index1 = F)
 
 terms = read.table('terms.txt')
-cvfit <- cv.glmnet(x = X_sp, y = realy, alpha=1, lower.limit=0, nfolds=5)
+cvfit <- cv.glmnet(x = X_sp, y = realy, alpha=1, nfolds=5)
 fit <- cvfit$glmnet.fit
 
-var <- which(fit$beta[,which(cvfit$lambda == cvfit$lambda.min)]>0)
+var <- which(fit$beta[,which(cvfit$lambda == cvfit$lambda.min)]!=0)
 var <- as.vector(var[var-length(realy)>0])
 terms$coef = 0
 terms[var-length(realy),]$coef =  fit$beta[,which(cvfit$lambda == cvfit$lambda.min)][var]
@@ -27,10 +27,10 @@ X = X[(length(realy) +1):dim(X)[1], ]
 X_sp = sparseMatrix(X[,1], X[,2], index1 = F)
 
 terms = read.table('terms.txt')
-cvfit <- cv.glmnet(x = X_sp, y = realy, alpha=1, lower.limit=0, nfolds=5)
+cvfit <- cv.glmnet(x = X_sp, y = realy, alpha=1, nfolds=5)
 fit <- cvfit$glmnet.fit
 
-var <- which(fit$beta[,which(cvfit$lambda == cvfit$lambda.min)]>0)
+var <- which(fit$beta[,which(cvfit$lambda == cvfit$lambda.min)]!=0)
 terms$coef = 0
 terms[var,]$coef =  fit$beta[,which(cvfit$lambda == cvfit$lambda.min)][var]
 terms$coef = format(terms$coef, digits=5)
